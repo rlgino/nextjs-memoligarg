7f844de75979a0847720f4c39b3f8539ca3f4c8d
Gino Luraschi <gino.luraschi@outlook.com.ar>
2023-11-21 12:19:07 +1300

    feat: Add tag manager

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Table from './components/table';
+import Script from 'next/script';
 
 export default function Home() {
+  const tagID = process.env.TAG_ID
   return (
     <div>
       <Head>
         <title>Memoligarg</title>
         <link rel="icon" href="/afaicon.svg" />
+
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${tagID}`}></Script>
+        <Script>
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${tagID}');
+            `
+          }
+        </Script>
       </Head>
 
       <div className={styles.app}>
